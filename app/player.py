0cc23205ab1cc7e2fa68c29e177166b6d567be46
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-08-24 21:21:42 +0800

    Doc: Added comments to player.py

--- app/player.py
+++ app/player.py
@@ -9,22 +9,26 @@ random.shuffle(pearson_table)
 
 class Player:
     def __init__(self, unique_id: str, player_name: str):
+        #Store the players ID and name/
         self.__unique_id = unique_id
         self.__player_name = player_name
 
 
     @property
     def uid(self):
+        #Return the players unique ID
         return self.__unique_id
 
 
     @property
     def player_name(self):
+        #Return the players current name
         return self.__player_name
 
 
     @player_name.setter
     def player_name(self, new_name):
+        #Update the players name
         self.__player_name = new_name
 
 
@@ -41,14 +45,19 @@ class Player:
 
 
     def __hash__(self) -> int:
+        #Generates the players hash using their unique ID
         return self.hash(self.uid)
 
 
     def __eq__(self, other):
+        #Only compare this object with another player
         if not isinstance(other, Player):
             return NotImplemented
+
+        #Players are equal when their unique IDs match
         return self.uid == other.uid
 
 
     def __str__(self):
+        #Returns a readable description of the player
         return f"player ID: {self.__unique_id}, player name: {self.__player_name}"
