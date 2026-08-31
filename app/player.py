827fa13d80f4423562ace13789b0332cb89b396d
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-08-31 14:08:00 +0800

    feat: Added scores to player

--- app/player.py
+++ app/player.py
@@ -8,29 +8,41 @@ random.shuffle(pearson_table)
 
 
 class Player:
-    def __init__(self, unique_id: str, player_name: str):
-        #Store the players ID and name/
+    def __init__(self, unique_id: str, player_name: str, score: int = 0):
+        # Store the player's ID and name.
         self.__unique_id = unique_id
         self.__player_name = player_name
 
+        # Store the player's score.
+        self.__score = score
 
     @property
     def uid(self):
-        #Return the players unique ID
+        # Return the player's unique ID.
         return self.__unique_id
-
 
     @property
     def player_name(self):
-        #Return the players current name
+        # Return the player's current name.
         return self.__player_name
-
 
     @player_name.setter
     def player_name(self, new_name):
-        #Update the players name
+        # Update the player's name.
         self.__player_name = new_name
 
+    @property
+    def score(self):
+        # Return the player's score.
+        return self.__score
+
+    @score.setter
+    def score(self, new_score):
+        # Score must be a positive integer.
+        if not isinstance(new_score, int) or new_score <= 0:
+            raise ValueError("Score must be a positive integer.")
+
+        self.__score = new_score
 
     @classmethod
     def hash(cls, key: str) -> int:
@@ -43,21 +55,26 @@ class Player:
 
         return hash_value
 
-
     def __hash__(self) -> int:
-        #Generates the players hash using their unique ID
+        # Generate the player's hash using their unique ID.
         return self.hash(self.uid)
 
-
     def __eq__(self, other):
-        #Only compare this object with another player
+        # Only compare this object with another player.
         if not isinstance(other, Player):
             return NotImplemented
 
-        #Players are equal when their unique IDs match
+        # Players are equal when their unique IDs match.
         return self.uid == other.uid
 
-
     def __str__(self):
-        #Returns a readable description of the player
+        # Return a readable description of the player.
         return f"player ID: {self.__unique_id}, player name: {self.__player_name}"
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"name='{self.player_name}', "
+            f"uid='{self.uid}', "
+            f"score={self.score})"
+        )
